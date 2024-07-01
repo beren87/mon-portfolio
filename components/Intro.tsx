7047bcf548c49b6d40e565ed749af8f16a87e467
berenger ferguenis <berenger.ferguenis@g5-digital.com>
2024-07-01 15:56:00 +0200

    changed Statut in Intro page

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -1,9 +1,9 @@
-"use client"
-import React from "react"
-import Image from "next/image"
-import Link from "next/link"
-import { Download, Mail, ScrollText } from "lucide-react"
-import { Button } from "@/components/ui/button"
+'use client';
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { Download, Mail, ScrollText } from 'lucide-react';
+import { Button } from '@/components/ui/button';
 
 export const Intro = () => {
   return (
@@ -13,7 +13,7 @@ export const Intro = () => {
           className='rounded-full border-2 border-primary'
           width={158}
           height={158}
-          src={"/berenger.jpg"}
+          src={'/berenger.jpg'}
           alt='Bérenger Ferguenis portrait'
           quality={100}
           priority={true}
@@ -22,14 +22,13 @@ export const Intro = () => {
         <h1 className='text-3xl font-bold py-3'>Bérenger Ferguenis</h1>
         <h2 className='pb-3 italic text-lg'>Développeur Web Junior</h2>
         <h3 className='pb-3'>
-          Statut :{" "}
+          Statut :{' '}
           <span className='text-yellow-200 font-semibold'>
-            En immersion du 17/06 au 28/06 2024 chez :
+            En formation professionnel du 01/07 au 29/08 2024 chez :
             <Link
               className='flex flex-row justify-center'
               href='https://g5-digital.com/'
-              target='_blank'
-            >
+              target='_blank'>
               G5 Digital
             </Link>
           </span>
@@ -39,15 +38,13 @@ export const Intro = () => {
             <Link
               href='https://github.com/beren87'
               target='_blank'
-              className='flex items-center'
-            >
+              className='flex items-center'>
               <svg
                 viewBox='0 0 24 24'
                 height='18'
                 width='18'
                 className='fill-svgbutton'
-                xmlns='http://www.w3.org/2000/svg'
-              >
+                xmlns='http://www.w3.org/2000/svg'>
                 <path d='m12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61-.546-1.385-1.335-1.755-1.335-1.755-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57 4.801-1.574 8.236-6.074 8.236-11.369 0-6.627-5.373-12-12-12' />
               </svg>
               <span className='text-secondary ml-1'>Github</span>
@@ -57,15 +54,13 @@ export const Intro = () => {
             <Link
               href='https://www.linkedin.com/in/berenger-ferguenis/'
               target='_blank'
-              className='flex'
-            >
+              className='flex'>
               <svg
                 viewBox='0 0 24 24'
                 height={18}
                 width={18}
                 className='fill-svgbutton'
-                xmlns='http://www.w3.org/2000/svg'
-              >
+                xmlns='http://www.w3.org/2000/svg'>
                 <path
                   d='m0 0h24v24h-24z'
                   fill='#fff'
@@ -87,9 +82,8 @@ export const Intro = () => {
             <a
               href='/CV- alternance -FERGUENIS-BERENGER.pdf'
               download
-              className='flex items-center'
-            >
-              <ScrollText className='mr-2 h-4 w-4 text-secondary' />{" "}
+              className='flex items-center'>
+              <ScrollText className='mr-2 h-4 w-4 text-secondary' />{' '}
               <span className='text-secondary'>Mon CV </span>
               <Download className='ml-2 h-4 w-4 text-secondary' />
             </a>
@@ -98,8 +92,7 @@ export const Intro = () => {
           <Button className='hover:scale-110 active:scale-105 transition rounded-full font-normal h-7 w-auto border-secondary bg-accent hover:bg-secondary-foreground border-2'>
             <Link
               href='mailto:berenger.fgs@gmail.com'
-              className='flex items-center'
-            >
+              className='flex items-center'>
               <Mail className='mr-2 h-4 w-4 text-secondary' />
               <span className='text-secondary'>Me contacter</span>
             </Link>
@@ -107,5 +100,5 @@ export const Intro = () => {
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
